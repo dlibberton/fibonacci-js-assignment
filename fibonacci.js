087bf489cd52c9a11a48df80a51d761036b768b1
dlibberton <drew.libberton@gmail.com>
2024-01-31 16:35:24 -0500

    initial solution for testing

--- fibonacci.js
+++ fibonacci.js
@@ -1,5 +1,15 @@
-function fibonacci(num) { changes
-  return 0;
+let numOne = 0;
+let numTwo = 1;
+let nTh;
+
+function fibonacci(num) {
+  for (let i = 2; i <= num; i++){
+    nTh = numOne + numTwo;
+    numOne = numTwo;
+    numTwo = nTh;
+  }
+
+  return nTh;
 }
 
 module.exports = fibonacci;
